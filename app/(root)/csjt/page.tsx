e8180d1b32db7c0df46843d021bd411f05d03336
stevendemer <devil.jaws@gmail.com>
2024-11-13 01:32:17 +0200

    minor changes to aim page

--- app/(root)/csjt/page.tsx
+++ app/(root)/csjt/page.tsx
@@ -4,68 +4,69 @@ import leaf from "@/public/assets/leaf.svg";
 import CSJTCard from "@/components/cards/csjt-card";
 import SubmitButton from "@/components/submit-button";
 import lamp from "@/public/assets/lamp.png";
+import hands from '@/public/assets/hands.png'
+import sunroof from "@/public/assets/sunroof.png"
 import Link from "next/link";
 
 export default function ConvinceJoinPage() {
-  return (
-    <div className="flex flex-col items-center justify-center w-full h-full max-w-3xl gap-4 relative flex-shrink overflow-hidden">
-      <div className="flex flex-col items-center gap-4 max-h-[17rem] h-full w-full">
-        <div className="relative w-full lg:max-w-lg mx-auto h-full">
-          <Image
-            className="object-contain object-center"
-            src={user}
-            alt=""
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Adjust image size for responsive
-          />
-        </div>
+    return (
+        <div
+            className="flex flex-col items-center justify-center w-full h-full max-w-3xl gap-4 relative flex-shrink overflow-hidden p-4">
+            <div className="flex flex-col items-center gap-4 max-h-[17rem] h-full w-full">
+                <div className="relative w-full lg:max-w-lg mx-auto h-full">
+                    <Image
+                        className="object-contain object-center"
+                        src={user}
+                        alt=""
+                        fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Adjust image size for responsive
+                    />
+                </div>
 
-        <p className="font-mulish whitespace-normal text-sm italic font-bold p-4 text-black">
-          {" "}
-          Your user looks like an{" "}
-        </p>
-      </div>
-      <div className="flex justify-center items-center w-full flex-shrink space-x-4 relative">
-        <Image
-          className="object-contain object-center transform scale-x-[-1]"
-          alt=""
-          width={50}
-          height={20}
-          src={leaf}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Adjust image size for responsive
-        />
-        <h2 className="font-semibold font-sans text-3xl text-black tracking-wide">
-          Environmental Tech Enthusiast
-        </h2>
-        <Image
-          className="object-cover object-center"
-          alt=""
-          width={50}
-          height={20}
-          src={leaf}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Adjust image size for responsive
-        />
-      </div>
-      <div className="flex items-center  w-full p-6 flex-shrink-0">
-        <div className="grid gap-4 md:grid-cols-3 w-full">
-          {/* 3 cards go here */}
-          <CSJTCard src={lamp}>
-            He&apos;s focused on minimising the impact of energy production and
-            consumption.
-          </CSJTCard>
-          <CSJTCard src={lamp}>
-            He&apos;s focused on minimising the impact of energy production and
-            consumption.
-          </CSJTCard>
-          <CSJTCard src={lamp}>
-            He&apos;s focused on minimising the impact of energy production and
-            consumption.
-          </CSJTCard>
+                <p className="font-mulish whitespace-normal text-sm italic font-bold p-4 text-black">
+                    {" "}
+                    Your user looks like an{" "}
+                </p>
+            </div>
+            <div className="flex justify-center items-center w-full flex-shrink space-x-4 relative">
+                <Image
+                    className="object-contain object-center transform scale-x-[-1]"
+                    alt=""
+                    width={50}
+                    height={20}
+                    src={leaf}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Adjust image size for responsive
+                />
+                <h2 className="font-semibold font-sans text-3xl text-black tracking-wide">
+                    Environmental Tech Enthusiast
+                </h2>
+                <Image
+                    className="object-cover object-center"
+                    alt=""
+                    width={50}
+                    height={20}
+                    src={leaf}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Adjust image size for responsive
+                />
+            </div>
+            <div className="flex items-center w-full p-6 flex-shrink-0">
+                <div className="grid gap-4 md:grid-cols-3 w-full">
+                    {/* 3 cards go here */}
+                    <CSJTCard src={lamp}>
+                        He&apos;s focused on minimising the impact of energy production and
+                        consumption.
+                    </CSJTCard>
+                    <CSJTCard src={sunroof}>
+                        He&apos;s all about sustainable living and clean energy
+                    </CSJTCard>
+                    <CSJTCard src={hands}>
+                        Always on the lookout for ways to shrink his ecological footprint
+                    </CSJTCard>
+                </div>
+            </div>
+            <Link passHref href="/aim">
+                <SubmitButton>Continue</SubmitButton>
+            </Link>
         </div>
-      </div>
-      <Link passHref href="/aim">
-        <SubmitButton>Continue</SubmitButton>
-      </Link>
-    </div>
-  );
+    );
 }
--- components/cards/csjt-card.tsx
+++ components/cards/csjt-card.tsx
@@ -1,29 +1,29 @@
-import Image, { StaticImageData } from "next/image";
-import { PropsWithChildren, ReactNode } from "react";
-import { Card, CardContent, CardHeader } from "../ui/card";
+import Image, {StaticImageData} from "next/image";
+import {PropsWithChildren, ReactNode} from "react";
+import {Card, CardContent, CardHeader} from "../ui/card";
 
 export default function CSJTCard({
-  children,
-  src,
-}: {
-  children: ReactNode;
-  src: StaticImageData;
+                                     children,
+                                     src,
+                                 }: {
+    children: ReactNode;
+    src: StaticImageData;
 }) {
-  return (
-    <Card className="max-w-xl w-full h-full shadow-lg">
-      <CardContent className="flex flex-col items-center gap-8 p-6">
-        <div className="relative rounded w-full h-full aspect-auto">
-          <Image
-            className="rounded object-cover w-full h-full object-center"
-            src={src}
-            alt=""
-          />
-        </div>
+    return (
+        <Card className="max-w-3xl w-full h-full shadow-lg">
+            <CardContent className="flex flex-col items-center gap-8 p-6">
+                <div className="relative rounded w-full h-full aspect-video">
+                    <Image
+                        className="rounded object-cover w-full h-full object-center"
+                        src={src}
+                        alt=""
+                    />
+                </div>
 
-        <p className="font-mulish text-black whitespace-normal max-w-lg truncate w-full overflow-hidden tracking-wide">
-          {children}
-        </p>
-      </CardContent>
-    </Card>
-  );
+                <p className="font-mulish text-black whitespace-normal max-w-lg truncate w-full overflow-hidden tracking-wide">
+                    {children}
+                </p>
+            </CardContent>
+        </Card>
+    );
 }
